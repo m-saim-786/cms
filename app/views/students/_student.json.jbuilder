f0dc93781138ce6b9a4a77ef63d7db663dc3e486
Saim00786 <muhammad.saim@devsinc.com>
2021-11-13 20:31:41 +0500

    initial commit

---
+++ app/views/students/_student.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! student, :id, :name, :roll_no, :Bform, :contact, :age, :father_name, :user, :created_at, :updated_at
+json.url student_url(student, format: :json)
---
+++ db/migrate/20211113152618_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :roll_no
+      t.string :Bform
+      t.string :contact
+      t.date :age
+      t.string :father_name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
